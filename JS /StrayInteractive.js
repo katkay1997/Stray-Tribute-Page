d6cbb0a3f2253dfd744c82c0f80480733fcf5cf0
kateramckay <kateradmckay@gmail.com>
2022-11-14 21:50:30 -0500

    Tribute website for the video game Stray

---
+++ JS /StrayInteractive.js
@@ -0,0 +1,27 @@
+/*const headColor= document.getElementsById("head");
+headColor.style.color= "#EFB700" //"#00ff9f" -> ~ sky blue 
+*/ 
+
+/* const banner = document.getElementById("nav")
+banner.style.backgroundColor = "#EFB700"
+banner.style.height = "150px"
+banner.style.width = "100vw"
+banner.style.margin = "-10px"
+banner.style.display = "flex"
+banner.style.flexDirection = "column" 
+banner.style.justifyContent = "center"
+banner.style.alignItems = "center" 
+*/
+
+
+/* const catLogo = document.getElementById("logo");
+catLogo.style.width = "100px"
+catLogo.style.alignItems = "center" */
+
+ /* const rooftop = document.getElementById("rooftop")
+ rooftop.style.alignItems ="center" */
+ 
+// const textBlock = document.getElementsByClassName("storyline")
+
+
+// traffic color - yellow #EFB700
